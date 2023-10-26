3b84c01ca608671f5dc3ab4df11e46e740281fbc
OmarSha30 <odessouk@gmail.com>
2023-10-26 16:56:23 -0400

    update

--- p1.py
+++ p1.py
@@ -1,11 +1,22 @@
 import socket
 import struct
 import threading
+import random
+import time
 
 def dns_query(hostname):
+    #header
+    id=random.randint(0, 65535)
+    qr=0
+
+
+    #question 
+
+
     pass
 
 def send_query():
+    port = 8888
     pass
 
 def recieve_response():
